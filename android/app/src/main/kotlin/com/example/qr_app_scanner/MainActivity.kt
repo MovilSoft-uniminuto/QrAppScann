e2c60a3c8350b6198bab728b4d852394da4d3e7c
clavijo99 <bclavijogomez@gmail.com>
2026-03-06 15:44:52 -0500

    Add Windows runner support with console output and DPI awareness
    - Introduced a manifest file for the Windows runner to enable DPI awareness.
    - Implemented console creation and output redirection in utils.cpp.
    - Added functions to handle command line arguments and UTF-16 to UTF-8 string conversion.
    - Created a Win32Window class to manage window creation, message handling, and theme updates.
    - Implemented dark mode support based on system preferences.
    - Added header files for utility functions and the Win32Window class.

---
+++ android/app/src/main/kotlin/com/example/qr_app_scanner/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.qr_app_scanner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_app_scann/const/theme/theme.dart';
+import 'package:qr_app_scann/presentacion/providers/theme_provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => ThemeProvider())],
+      builder: (context, child) {
+        final theme = Provider.of<ThemeProvider>(context);
+        return Builder(
+          builder: (context) {
+            return MaterialApp(
+              title: 'Flutter Demo',
+              theme: theme.isDark ? themeDark : themeLight,
+              home: Scaffold(),
+            );
+          },
+        );
+      },
+    );
+  }
+}
